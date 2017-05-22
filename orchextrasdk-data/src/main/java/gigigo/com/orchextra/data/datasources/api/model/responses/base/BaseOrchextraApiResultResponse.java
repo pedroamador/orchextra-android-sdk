94da823324aee9749afd0d7f2ac454cb8f8b5e5a
RuiAlonso <rui.alonso@gigigo.com>
2017-05-22 10:15:32 +0200

    added ApiErrorResponse and ApiResultResponse to model responses

---
+++ orchextrasdk-data/src/main/java/gigigo/com/orchextra/data/datasources/api/model/responses/base/BaseOrchextraApiResultResponse.java
@@ -0,0 +1,44 @@
+/*
+ * Created by Orchextra
+ *
+ * Copyright (C) 2016 Gigigo Mobile Services SL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package gigigo.com.orchextra.data.datasources.api.model.responses.base;
+
+import com.gigigo.ggglib.network.responses.ApiGenericResultResponse;
+import com.google.gson.annotations.SerializedName;
+
+public class BaseOrchextraApiResultResponse<JSONData> extends ApiGenericResultResponse<JSONData> {
+
+  @SerializedName("status") private boolean status;
+  @SerializedName("data") private JSONData data;
+
+  public boolean getStatus() {
+    return status;
+  }
+
+  public void setStatus(boolean status) {
+    this.status = status;
+  }
+
+  @Override public JSONData getResult() {
+    return data;
+  }
+
+  @Override public void setResult(JSONData data) {
+    this.data = data;
+  }
+}
